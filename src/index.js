1199a0a86a2e2ed68749743f9954e742b662e2eb
JonathanAllisson <jonathan.allisson.silva@ccc.ufcg.edu.br>
2022-06-27 11:06:16 -0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import path from 'path';
 import { router } from './routes.js';
 
 const app = express();
@@ -17,13 +18,15 @@ if (process.env.NODE_ENV === 'production') {
     // js and css files
     app.use(express.static('client/build'));
 
-    // index.html for all page routes
-    const path = require('path');
+    
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-app.listen(5000, () => {
-    console.log('Listen in port 5000');
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('Listen in port ' + process.env.PORT);
 });
