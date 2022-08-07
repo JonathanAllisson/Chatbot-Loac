7e70b956a8142a222d75083056cd3c5c3739e526
JonathanAllisson <jonathan.allisson.silva@ccc.ufcg.edu.br>
2022-08-07 19:52:04 -0300

    updates

--- client/src/util/formatMsg.js
+++ client/src/util/formatMsg.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-export const addLineBreaks = string =>
-string.split('\n').map((text, index) => (
-    <React.Fragment key={`${text}-${index}`}>
-        {text}
-        <br />
-    </React.Fragment>
+export const addLineBreaks = string => 
+    string.split('\n').map((text, index) => (
+        <React.Fragment key={`${text}-${index}`}>
+            {formatcolors(text)}
+            <br />
+        </React.Fragment>
 ));
+
+const formatcolors = t => {
+    if(t.includes('-f*')){
+        let textArray = t.split('-f*');
+        if(textArray.length < 2) return t;
+        return <React.Fragment> <span className='negrito'> {textArray[1]} </span> {textArray[2]} </React.Fragment>;
+    }
+    else if(t.includes('-frd')){
+        let textArray = t.split('-frd');
+        if(textArray.length < 2) return t;
+        return <React.Fragment> <span className='red'> {textArray[1]} </span> {textArray[2]} </React.Fragment>;
+    }
+    else if(t.includes('-fbl')){
+        let textArray = t.split('-fbl');
+        if(textArray.length < 2) return t;
+        return <React.Fragment> <span className='blue'> {textArray[1]} </span> {textArray[2]} </React.Fragment>;
+    }
+    else if(t.includes('-fgr')){
+        let textArray = t.split('-fgr');
+        if(textArray.length < 2) return t;
+        return <React.Fragment> <span className='green'> {textArray[1]} </span> {textArray[2]} </React.Fragment>;
+    }
+    else{
+        return t;
+    }
+    
+}
